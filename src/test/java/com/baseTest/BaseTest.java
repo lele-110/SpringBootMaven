fd371d9e879483cbde0365525f391700bdef7479
hefule <358139152@qq.com>
2016-08-02 22:21:29 +0800

    junit测试更改

---
+++ src/test/java/com/baseTest/BaseTest.java
@@ -0,0 +1,18 @@
+package com.baseTest;
+
+import com.ApplicationMain;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * 测试代码基本类
+ *@author hefule
+ *@date 2016/8/2 13:17
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ApplicationMain.class)
+@WebAppConfiguration
+public class BaseTest {}
--- src/test/java/com/test/test.java
+++ src/test/java/com/test/test.java
@@ -1,24 +1,17 @@
 package com.test;
 
-import com.ApplicationMain;
+import com.baseTest.BaseTest;
 import com.service.demo.DemoService;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
- * 测试代码
+ * 测试代码样例
  *@author hefule
  *@date 2016/8/2 13:17
  *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ApplicationMain.class)
-@WebAppConfiguration
-public class test {
+public class test extends BaseTest {
 
     @Autowired
     private DemoService userService;
