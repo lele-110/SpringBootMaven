0e8a6cc5127dd3ba50bdf857b0a3c23e08b51e19
hefule <358139152@qq.com>
2016-08-03 01:20:34 +0800

    前段框架更改

--- src/main/java/com/controller/demo/HomeAction.java
+++ src/main/java/com/controller/demo/HomeAction.java
@@ -6,6 +6,8 @@ import com.untils.framework.LoggerInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,15 +31,33 @@ public class HomeAction extends LoggerInfo<HomeAction>{
      *
      */
     @RequestMapping("/index")
+    //@ResponseBody  如果需要返回json则添加
     public String index(Model model) {
         try {
             model.addAttribute("name","dddd");
             model.addAttribute("demo",demoService.getUserInfo());
-            Demo demo = (Demo) redisUtil.loadObject("getUserInfo");
-            System.out.println(demo.getName());
+            Demo demo3 = (Demo) redisUtil.loadObject("getUserInfo");
         } catch (Exception e) {
             e.printStackTrace();
         }
         return "index";
+    }
+
+    /**
+     *  自定义错误页面
+     *  @author hefule
+     *  @date 2016/8/3 0:48
+     *
+     */
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+        return (container -> {
+          /*  ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+
+            container.addErrorPages(error401Page, error404Page, error500Page);*/
+        });
     }
 }
