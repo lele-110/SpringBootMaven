642f95724fe66b191b25abe5882344dd8746717d
hefule <358139152@qq.com>
2016-08-02 22:13:25 +0800

    部分引用更改，加入velocity模板

--- src/test/java/com/test/test.java
+++ src/test/java/com/test/test.java
@@ -1,8 +1,13 @@
 package com.test;
 
+import com.ApplicationMain;
 import com.service.demo.DemoService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * 测试代码
@@ -10,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
  *@date 2016/8/2 13:17
  *
  */
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ApplicationMain.class)
+@WebAppConfiguration
 public class test {
 
     @Autowired
@@ -18,7 +25,6 @@ public class test {
 
     @Test
     public void first(){
-
         try {
             System.out.println(userService.getUserInfo());
         } catch (Exception e) {
