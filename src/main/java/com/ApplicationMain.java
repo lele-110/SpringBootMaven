96462c93a09d9ae68b15b753c108800084624bfe
hefule <358139152@qq.com>
2016-08-02 14:06:07 +0800

    更改静态类

--- src/main/java/com/ApplicationMain.java
+++ src/main/java/com/ApplicationMain.java
@@ -1,24 +1,13 @@
 package com;
 
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 /**
  * Created by hefule on 2016/7/30.
@@ -27,7 +16,8 @@ import javax.sql.DataSource;
 @EnableAutoConfiguration//启用自动配置
 @ComponentScan//组件扫描
 public class ApplicationMain extends SpringBootServletInitializer {
-    private Logger logger = LogManager.getLogger(ApplicationMain.class);
+    private static Class<ApplicationMain> applicationClass = ApplicationMain.class;
+    private Logger logger = LogManager.getLogger(applicationClass);
 
     /**
      * 打包成war时候需要用到，即指定入口
@@ -36,7 +26,7 @@ public class ApplicationMain extends SpringBootServletInitializer {
      */
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(ApplicationMain.class);
+        return builder.sources(applicationClass);
     }
 
     /**
@@ -45,6 +35,6 @@ public class ApplicationMain extends SpringBootServletInitializer {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(ApplicationMain.class,args);
+        SpringApplication.run(applicationClass,args);
     }
 }
